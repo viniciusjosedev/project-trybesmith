55e6a6c1b4ddefe5f95ad217238bc26cd93dcb1d
Vinicius José <viniciusjosedev@gmail.com>
2023-05-28 23:41:26 -0300

    fix(schema.ts): error lint fix

--- src/middlewares/validations/schema.ts
+++ src/middlewares/validations/schema.ts
@@ -1,7 +1,5 @@
 import Joi, { ObjectSchema } from 'joi';
 
-const MESSAGE = '"username" and "password" are required';
-
 const userLoginSchema: ObjectSchema = Joi.object({
   username: Joi.string().min(1).required(),
   password: Joi.string().min(1).required(),
